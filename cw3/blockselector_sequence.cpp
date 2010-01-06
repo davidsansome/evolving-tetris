6fd917c7dae662955dd4061f84798a4faa9ffec4
David Sansome <me@davidsansome.com>
2010-01-06 18:50:43 +0000

    Lower the default selector mutation rate
    git-svn-id: file:///home/david/svn/comp6026@56 ba5342e2-1382-4ac7-b7c6-837a19329574

--- cw3/blockselector_sequence.cpp
+++ cw3/blockselector_sequence.cpp
@@ -1,3 +1,3 @@
 #include "blockselector_sequence.h"
 
-DEFINE_double(smrate, 0.01, "probability of a block selector gene being mutated");
+DEFINE_double(smrate, 0.0001, "probability of a block selector gene being mutated");
